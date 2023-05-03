d43442e1d8ca2d57c4e445ca3536a4878095436c
Sche <scheliganmonzon1@gmail.com>
2023-05-03 11:08:46 -0300

    juntando as alterações feitas

--- src/main/java/br/com/Sublimoon/PI/entity/Cliente.java
+++ src/main/java/br/com/Sublimoon/PI/entity/Cliente.java
@@ -4,11 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.time.LocalDate;
-
-import java.math.BigDecimal;
-import java.util.List;
 @Entity
 @Table(name = "Clientes",schema = "public")
 public class Cliente extends Usuario {
@@ -21,7 +16,7 @@ public class Cliente extends Usuario {
     @Getter @Setter
     @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "Favoritos",nullable = false)
-    private Favoritos favoritos;
+    private Favorito favoritos;
 
     @Getter @Setter
     @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
--- src/main/java/br/com/Sublimoon/PI/repository/CategoriasRepository.java
+++ src/main/java/br/com/Sublimoon/PI/repository/CategoriasRepository.java
@@ -1,12 +1,9 @@
 package br.com.Sublimoon.PI.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.Sublimoon.PI.entity.Categorias;
+import br.com.Sublimoon.PI.entity.Categoria;
 import org.springframework.stereotype.Repository;
 
-
-import java.util.List;
-
 @Repository
-public interface CategoriasRepository extends JpaRepository<Categorias,Long>{
+public interface CategoriasRepository extends JpaRepository<Categoria,Long>{
 }
