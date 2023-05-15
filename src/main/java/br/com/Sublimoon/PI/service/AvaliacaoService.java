6dfda067a17d2d1bd2da5627d533efcf562a7112
regi <reginaldosgomesj@gmail.com>
2023-05-15 19:16:31 -0300

    Service avaliacao feita

--- src/main/java/br/com/Sublimoon/PI/service/AvaliacaoService.java
+++ src/main/java/br/com/Sublimoon/PI/service/AvaliacaoService.java
@@ -8,6 +8,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 
+import java.util.Optional;
+
 @Service
 public class AvaliacaoService {
 
@@ -17,7 +19,17 @@ public class AvaliacaoService {
     @Transactional(rollbackFor = Exception.class)
     public void createAvaliacao(final Avaliacao avaliacao){
 
-    
+    Assert.isTrue(avaliacao.getId() != null, "Id não pode ser nulo");
+    Optional<Avaliacao> avaliacaoExistente = AvaliacaoRep.findById(avaliacao.getId());
+    Assert.isTrue(avaliacaoExistente == null || avaliacaoExistente.equals(avaliacao.getId()), "Id já existente");
+
+    Assert.isTrue(avaliacao.getNota() != null,"Nota não pode ser nulo");
+
+    Assert.isTrue(avaliacao.getCliente() != null,"Cliente não pode ser nulo");
+
+    Assert.isTrue(!avaliacao.getComentario().equals(""), "Comentario não pode ser nulo");
+    Assert.isTrue(avaliacao.getComentario().length() <= 150, "Comentário deve conter até 150 caracteres");
+
 
     }
 
