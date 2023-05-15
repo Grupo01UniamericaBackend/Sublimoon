05c5e34dac1ae34df35eadb32eb795ae7ca4f88b
Gabriel S. A <falconrei123@gmail.com>
2023-05-15 19:24:36 -0300

    Teste retorto da branch e Link EnvioService

--- Sublimoon/src/main/java/br/com/Sublimoon/PI/service/EnvioService.java
+++ Sublimoon/src/main/java/br/com/Sublimoon/PI/service/EnvioService.java
@@ -1,4 +1,10 @@
 package br.com.Sublimoon.PI.service;
 
+import br.com.Sublimoon.PI.repository.EnvioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EnvioService {
+
+    @Autowired
+    EnvioRepository envioRepository;
 }
