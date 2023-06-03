69b1928a3bde4726af37372d441ef7a4896a1efd
Gabriel S. A <falconrei123@gmail.com>
2023-06-03 15:00:54 -0300

    catch (lugar onde cai se der errado) do metodo Post em Produto modificado

--- src/main/java/br/com/Sublimoon/PI/controller/ProdutoController.java
+++ src/main/java/br/com/Sublimoon/PI/controller/ProdutoController.java
@@ -41,8 +41,8 @@ public class ProdutoController {
             this.produtoRepository.save(produto);
             return ResponseEntity.ok("Registro cadastrado com sucesso");
         }
-        catch (DataIntegrityViolationException e){
-            return ResponseEntity.internalServerError().body("Error: " + e.getCause().getCause().getMessage());
+        catch (Exception e){
+            return ResponseEntity.internalServerError().body("Error: " + e.getMessage());
         }
     }
 
