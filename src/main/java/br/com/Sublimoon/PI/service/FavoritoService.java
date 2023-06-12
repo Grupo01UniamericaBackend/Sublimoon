0fc4ccde7e13a6fecd23af2f575eed1d691898b4
Scheligan <scheliganmonzon1@gmail.com>
2023-06-12 13:57:16 -0300

    arrumando favoritos, agora consultando os dados de cliente

--- src/main/java/br/com/Sublimoon/PI/service/FavoritoService.java
+++ src/main/java/br/com/Sublimoon/PI/service/FavoritoService.java
@@ -3,6 +3,7 @@ package br.com.Sublimoon.PI.service;
 
 import br.com.Sublimoon.PI.entity.Favorito;
 import br.com.Sublimoon.PI.entity.Produto;
+import br.com.Sublimoon.PI.repository.ClienteRepository;
 import br.com.Sublimoon.PI.repository.FavoritoRepository;
 import br.com.Sublimoon.PI.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,34 +18,43 @@ import java.util.List;
 public class FavoritoService{
     @Autowired final FavoritoRepository favoritoRepository;
     @Autowired final ProdutoRepository produtoRepository;
+    @Autowired final ClienteRepository clienteRepository;
 
-    public FavoritoService(FavoritoRepository favoritoRepository, ProdutoRepository produtoRepository) {
+    public FavoritoService(FavoritoRepository favoritoRepository, ProdutoRepository produtoRepository, ClienteRepository clienteRepository) {
         this.favoritoRepository = favoritoRepository;
         this.produtoRepository = produtoRepository;
+        this.clienteRepository = clienteRepository;
     }
 
 
     @Transactional(rollbackFor = Exception.class)
     public Favorito Favoritar(final Favorito favorito) {
 
-
+        Long idCliente = favorito.getCliente().getId();
         Long produtoId = favorito.getProdutoId();
 
+        Assert.isTrue(clienteRepository.findById(idCliente)!=null, "cliente não encontrado");
         Assert.isTrue(produtoRepository.findById(produtoId).get()!= null, "Produto não encontrado!");
 
-
-
-
+        favorito.setCliente(clienteRepository.getById(idCliente));
 
         if(favorito.getProdutos()==null) {
 
             List<Produto> attProduto = new ArrayList<>(); // Cria uma nova lista caso ainda não exista
+            attProduto.add(produtoRepository.getById(produtoId));
             favorito.setProdutos(attProduto); // Define a lista no favorito
         }
         else {
 
             favorito.getProdutos().add(produtoRepository.getById(produtoId)); // Adiciona o produto à lista de produtos
         }
+
+
+
+
+
+
+
         return favoritoRepository.save(favorito);
 
     }
