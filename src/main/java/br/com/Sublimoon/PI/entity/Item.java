ba869454401238df15ee8b735f55e135bd4db2ec
Scheligan <scheliganmonzon1@gmail.com>
2023-06-21 23:55:34 -0300

    função addCarrinho adicionada, modificação em AdicionarItem, modificação em CreateCarrinho, e agora é para o valor total estar sendo atualizado no for de itens

--- src/main/java/br/com/Sublimoon/PI/entity/Item.java
+++ src/main/java/br/com/Sublimoon/PI/entity/Item.java
@@ -1,34 +1,29 @@
 package br.com.Sublimoon.PI.entity;
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import java.math.MathContext;
+
+
 @Entity
 @Table(name = "itemCarrinho", schema = "public")
 public class Item extends AbstractEntity {
 
     @Getter @Setter
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "produto")
+    @JoinColumn(name = "produto",nullable = false)
     private Produto produto;
+
     @Getter @Setter
     @Column(name = "quantidade", nullable = false)
     private int quantidade;
     @Getter @Setter
-    @Column(name = "valor", nullable = false)
+    @Column(name = "valor")
     private float valor;
     @Getter @Setter
-    @Column(name = "valorUnit", nullable = false)
+    @Column(name = "valorUnit")
     private float valorUnit;
-
-    @Getter
+    @Getter @Setter
     @Column(name = "valorTotal")
     private float ValorTotal = valorUnit * quantidade;
-
-
-
-
-
 
 }
--- src/main/java/br/com/Sublimoon/PI/service/CarrinhoService.java
+++ src/main/java/br/com/Sublimoon/PI/service/CarrinhoService.java
@@ -5,10 +5,12 @@ import br.com.Sublimoon.PI.entity.Item;
 import br.com.Sublimoon.PI.entity.Produto;
 import br.com.Sublimoon.PI.repository.CarrinhoRepository;
 import br.com.Sublimoon.PI.repository.ProdutoRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,62 +28,93 @@ public class CarrinhoService {
 
     @Transactional(rollbackFor = Exception.class)
     public Carrinho createCarrinho(final Carrinho carrinho){
-        float quantidadeTotal = 0;
+       /* float quantidadeTotal = 0;
 
         float subTotal = 0;
-
-        float teste = carrinho.getProdutos().get(1).getQuantidadeProCarrinho();
-
-        System.out.println(teste);
-
-        /*for(int i = 0; i < carrinho.getProdutos().size(); i ++){
-
+       // float teste = carrinho.getProdutos().get(1).getQuantidadeProCarrinho();
+       // System.out.println(teste);
+        for(int i = 0; i < carrinho.getProdutos().size(); i ++){
            quantidadeTotal += carrinho.getProdutos().get(i).getQuantidadeProCarrinho();
-
            subTotal += (carrinho.getProdutos().get(i).getQuantidadeProCarrinho() * carrinho.getProdutos().get(i).getPreco());
         }*/
-        for(int i = 0; i < carrinho.getProdutos().size(); i ++){
 
-            adicionarItem(carrinho.getProdutos().get(i),carrinho.getItens());
+
+
+
+        for(int i = 0; i < carrinho.getItem().size(); i ++){
+            Item itemNovo = carrinho.getItem().get(i);
+
+            Produto produto = itemNovo.getProduto();
+            float valorUnitario = produto.getPesoProduto();
+
+            itemNovo.setValorUnit(valorUnitario);
+            itemNovo.setValor(itemNovo.getValorTotal());
+
+            carrinho.setSubTotal(carrinho.getSubTotal() + itemNovo.getValorTotal());
+
         }
 
         return carrinhoRepo.save(carrinho);
     }
 
     @Transactional(rollbackFor = Exception.class)
-    public void adicionarItem(Produto produto, List<Item> Itens){
-        int i = -1;
-        float valorUnitario = produto.getPesoProduto();
-        int quantidade = produto.getQuantidadeProCarrinho();
+    public Carrinho addCarrinho(long id, final Carrinho carrinho){
 
-        for(int x = 0; x < getItens(Itens).size() & i < 0; i++){
-            Item itemAdd = Itens.get(x);
+            Carrinho carrinhoAntigo = carrinhoRepo.getById(id);
+            List<Item> CarrinhoNovo = adicionarItem(carrinhoAntigo.getItem(),carrinho.getItem());
 
-            if(itemAdd.getProduto().equals(produto)){
-                i = x;
-            }
-        }
-        try{
-            if(i < 0){
-                Item item = new Item();
-                item.setValor(item.getValorTotal());
-                getItens(Itens).add(item);
-            }
-            else{
-                Item itemAdd = Itens.get(i);
-                quantidade = itemAdd.getQuantidade() + quantidade;
-                valorUnitario = itemAdd.getValorUnit();
-                Item item = new Item();
-                item.setValor(item.getValorTotal());
+            BeanUtils.copyProperties(carrinho, carrinhoAntigo, "id");
 
-                Itens.set(i,item);
+            for(int i = 0; i < carrinho.getItem().size(); i ++){
+                Item itemNovo = carrinho.getItem().get(i);
 
+                Produto produto = itemNovo.getProduto();
+                float valorUnitario = produto.getPesoProduto();
+
+                itemNovo.setValorUnit(valorUnitario);
+                itemNovo.setValor(itemNovo.getValorTotal());
+                carrinho.setSubTotal(carrinho.getSubTotal() + itemNovo.getValorTotal());
 
             }
+
+
+        return carrinhoRepo.save(carrinho);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public List<Item> adicionarItem(List<Item> listaItens, List<Item> Itens){
+
+        for(int pos = 0; pos <= Itens.size(); pos++) {
+            int i = -1;
+            Item itemAdd = Itens.get(pos);
+            for (int x = 0; x < listaItens.size() & i < 0; x++) {
+
+                Produto produto = listaItens.get(x).getProduto();
+                if (itemAdd.getProduto().equals(produto)) {
+                    i = x;
+                }
+            }
+            try {
+                if (i < 0) { //se não encontrar nenhum item igual no carrinho ele adiciona o item
+
+                    listaItens.add(itemAdd);
+                }
+                else { //se encontrar um item igual, ele muda a quantidade e o valor
+                    itemAdd = Itens.get(i);
+                    int quantidade = itemAdd.getQuantidade() + listaItens.get(i).getQuantidade();//soma a quantidade
+
+                    itemAdd.setQuantidade(quantidade);
+
+                    listaItens.set(i, itemAdd);
+
+                }
+
+            }
+            catch (RuntimeException e) {
+                e.getStackTrace();
+            }
         }
-        catch (RuntimeException e){
-            e.getStackTrace();
-        }
+        return listaItens;
     }
     public Collection<Item> getItens(List<Item> Itens){
 
