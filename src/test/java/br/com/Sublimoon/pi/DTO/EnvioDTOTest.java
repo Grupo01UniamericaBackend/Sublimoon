5e1e4f03bb8ccf1401036d551bc247bb07487ea5
Gabriel S. A <falconrei123@gmail.com>
2023-09-29 13:35:52 -0300

    testes de entity e DTO de Evio feitos

--- src/test/java/br/com/Sublimoon/pi/DTO/EnvioDTOTest.java
+++ src/test/java/br/com/Sublimoon/pi/DTO/EnvioDTOTest.java
@@ -14,20 +14,30 @@ class EnvioDTOTest {
         envioDTO.setFormaEnvio("Lancha");
         Assertions.assertEquals("Lancha",envioDTO.getFormaEnvio());
     }
-
     @Test
-    void testEquals() {
+    void construtorVazioTest(){
+        EnvioDTO envioDTO3 = new EnvioDTO();
+        Assertions.assertEquals(envioDTO2,envioDTO3);
     }
-
-    @Test
-    void canEqual() {
-    }
-
     @Test
     void testHashCode() {
+        EnvioDTO envioDTO1 = new EnvioDTO("teste1",99);
+        EnvioDTO envioDTO2 = new EnvioDTO("teste1",99);
+        Assertions.assertEquals(envioDTO1.hashCode(),envioDTO2.hashCode());
     }
-
     @Test
     void testToString() {
+        EnvioDTO envioDTO1 = new EnvioDTO("TesteDTO", 99);
+        Assertions.assertEquals(envioDTO1.toString(),envioDTO.toString());
+    }
+    @Test
+    void comparacaoFormaDeEnvioTest(){
+        EnvioDTO envioDTO1 = new EnvioDTO("TesteDTO", 99);
+        Assertions.assertEquals(envioDTO.getFormaEnvio(),envioDTO1.getFormaEnvio());
+    }
+    @Test
+    void comapracaoValorFreteTest(){
+        EnvioDTO envioDTO1 = new EnvioDTO("TesteDTO", 99);
+        Assertions.assertEquals(envioDTO.getValorFrete(),envioDTO1.getValorFrete());
     }
 }
